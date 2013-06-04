82bd550f15868690ed037dbc71b65604f508d7f8
S515793 <S515793@NW21850.nwmissouri.edu>
2013-06-04 14:40:04 -0500

    I have added a print statement with my name. (Mohammed Jawad Shafi)

---
+++ src/gitassigment2/GitAssigment2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitassigment2;
+
+/**
+ *
+ * @author Mohammed Jawad Shafi
+ */
+public class GitAssigment2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Mohammed Jawad Shafi ");
+    }
+}
